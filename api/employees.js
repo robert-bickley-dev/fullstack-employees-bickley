80a3f3c6725a57e6f5eb461d65ed959965c612f7
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-04 17:37:16 -0400

    Add createEmployee import

--- api/employees.js
+++ api/employees.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 export default router;
 
-import { getEmployees } from "#db/queries/employees.js";
+import { getEmployees, createEmployee } from "#db/queries/employees.js";
 
 router.get("/", async (req, res) => {
   const employees = await getEmployees();

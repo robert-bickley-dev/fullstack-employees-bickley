3ebab7c42a564450be6eb454e82942b9fe3acd5d
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-04 17:34:31 -0400

    Add request body guard

--- api/employees.js
+++ api/employees.js
@@ -8,3 +8,10 @@ router.get("/", async (req, res) => {
   const employees = await getEmployees();
   res.send(employees);
 });
+
+router.post("/", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body is required");
+
+  const { name, birthday, salary } = req.body;
+  const employee = await createEmployee(name, birthday, salary);
+});

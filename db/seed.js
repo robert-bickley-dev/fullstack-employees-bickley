db13343ed0839f51cb36e3f14010b176082a5f6d
Rob Bickley <robert.john.bickley@gmail.com>
2026-06-03 21:12:22 -0400

    Import createEmployee query function to seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,6 @@
 import db from "#db/client";
 import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -7,15 +8,15 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  const randomName = faker.person.fullName();
-  const randomBirthday = faker.date.birthdate();
-  const randomSalary = faker.finance.amount({
-    min: 50000,
-    max: 200000,
-    dec: 0,
-  });
-
   for (let i = 1; i < 11; i++) {
+    const randomName = faker.person.fullName();
+    const randomBirthday = faker.date.birthdate();
+    const randomSalary = faker.finance.amount({
+      min: 50000,
+      max: 200000,
+      dec: 0,
+    });
+
     await createEmployee(randomName, randomBirthday, randomSalary);
   }
 }
